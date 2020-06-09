b3393ce6b67e4ae8763c70bb8286e49e3e69767d
ryanaanderson0 <ryanaanderson0@gmail.com>
2020-06-09 00:00:28 +0000

    added styles

--- react_tutorials/src/Header.js
+++ react_tutorials/src/Header.js
@@ -4,9 +4,7 @@ import React from "react"
 function Header() {
     return(
         <header className="navbar">
-            <nav>
                 <h1>This is the header portion!</h1>
-            </nav>
         </header>
     )
 }
